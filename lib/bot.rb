fd3e04a9f9aae36294f0178d175c55d01555e634
anapdh <ana_d_hubner@hotmail.com>
2020-12-17 19:45:54 -0300

    add last codes

--- lib/bot.rb
+++ lib/bot.rb
@@ -17,70 +17,89 @@ class Bot
 
         bot.api.send_message(chat_id: message.chat.id, text: 
           "Hello, #{message.from.first_name}!\n
-    Welcome to your Zodiac Info bot, created by Ana Paula Hübner!\n
-    How to use the bot:\n 
-    Use  /start to start or restart the bot\n 
-    Use /stop to end the bot\n 
-    Type '/' + the zodiac sign you wish to obtain information, or simply select the option below:\n
-    /aries    /taurus    /gemini    /cancer\n
-    /leo    /virgo    /libra    /scorpio\n
-    /sagittarius    /capricorn    /aquarius    /pisces")
+Welcome to Ana of Zodiacs, a Ruby project created by Ana Paula Hübner.\n
+Here's how to use the bot:\n 
+  • Use  /start to start or restart the bot\n 
+  • Use /stop to end the bot when you wish\n 
+  • Type ' / ' + the zodiac sign you wish to obtain information, or simply select one of the options below:\n
+      /aries    /taurus    /gemini    /cancer\n
+      /leo    /virgo    /libra    /scorpio\n
+      /sagittarius    /capricorn    /aquarius    /pisces")
 
-        when '/stop'
-
+        when '/stop' && '/no'
         bot.api.send_message(chat_id: message.chat.id, text: 
-          "See you next time, #{message.from.first_name}!" , date: message.date)
+          "Thanks for accessing, #{message.from.first_name}! See you next time :)" , date: message.date)
 
         when '/aries'
         @zodiac_sign.aries
         bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.aries, date: message.date)
-       
+        bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.repeat, date: message.date)
+
         when '/taurus'
         @zodiac_sign.taurus
         bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.taurus, date: message.date)
+        bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.repeat, date: message.date)
 
         when '/gemini'
         @zodiac_sign.gemini
         bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.gemini, date: message.date)
+        bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.repeat, date: message.date)
 
         when '/cancer'
         @zodiac_sign.cancer
         bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.cancer, date: message.date)
+        bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.repeat, date: message.date)
 
         when '/leo'
         @zodiac_sign.leo
         bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.leo, date: message.date)
+        bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.repeat, date: message.date)
 
         when '/virgo'
         @zodiac_sign.virgo
         bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.virgo, date: message.date)
+        bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.repeat, date: message.date)
 
         when '/libra'
         @zodiac_sign.libra
         bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.libra, date: message.date)
+        bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.repeat, date: message.date)
 
         when '/scorpio'
         @zodiac_sign.scorpio
         bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.scorpio, date: message.date)
+        bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.repeat, date: message.date)
 
         when '/sagittarius'
         @zodiac_sign.sagittarius
         bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.sagittarius, date: message.date)
+        bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.repeat, date: message.date)
 
         when '/capricorn'
         @zodiac_sign.capricorn
         bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.capricorn, date: message.date)
+        bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.repeat, date: message.date)
 
         when '/aquarius'
         @zodiac_sign.aquarius
         bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.aquarius, date: message.date)
+        bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.repeat, date: message.date)
 
         when '/pisces'
         @zodiac_sign.pisces
         bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.pisces, date: message.date)
+        bot.api.send_message(chat_id: message.chat.id, text: @zodiac_sign.repeat, date: message.date)
+        
+        when '/yes'
+        bot.api.send_message(chat_id: message.chat.id, text: 
+          "Alright, #{message.from.first_name}, type or select another zodiac sign:\n
+    /aries    /taurus    /gemini    /cancer\n
+    /leo    /virgo    /libra    /scorpio\n
+    /sagittarius    /capricorn    /aquarius    /pisces")
       
         else bot.api.send_message(chat_id: message.chat.id, text: 
-        "Invalid entry, #{message.from.first_name}! Please, use /start, /stop, or '/' + the zodiac sign of your choice.")
+        "Invalid entry!
+Please, use /start, /stop, or ' / ' + the zodiac sign of your choice.")
         end
       end
     end
--- lib/message.rb
+++ lib/message.rb
@@ -3,11 +3,11 @@ require_relative 'bot'
 
 # Class for the horoscope messages
 class ZodiacInfo
-  attr_reader :aries, :taurus, :gemini, :cancer, :leo, :virgo, :libra, :scorpio, :sagittarius, :capricorn, :aquarius, :pisces
+  attr_reader :aries, :taurus, :gemini, :cancer, :leo, :virgo, :libra, :scorpio, :sagittarius, :capricorn, :aquarius, :pisces, :repeat
 
   def initialize
     @aries = "You chose Aries! Let's see...\n
-Personality Traits: Energetic, candid and willful
+Personality Traits: energetic, candid and willful
 Date of Birth: March 21 - April 19
 Strength: hopeful, active, energetic, honest, versatile, brave, adventurous, passionate, generous, cheerful, argumentative, curious
 Weakness: impulsive, naive, self-willed, belligerent, impatient
@@ -22,6 +22,7 @@ Celebrities: Hans Christian Andersen, Jackie Chan, Mariah Carey, Marlon Brando, 
 Chinese zodiac twins: Dragon"
 
     @taurus = "You chose Taurus! Let's see...\n
+Personality Traits: reliable, diligent and conservative
 Strength: romantic, decisive, logical, diligent, ardent, patient, talented in art, perseverant, benevolent
 Weakness: prejudiced, dependent, stubborn
 Symbol: Bull
@@ -36,6 +37,7 @@ See Also: Taurus Horoscope in 2020
 Chinese zodiac twins: Snake"
 
     @gemini = "You chose Taurus! Let's see...\n
+Personality Traits: quick-witted, capricious and cheerful
 Strength: multifarious, perspicacious, smart, cheerful, quick-witted, clement, charming
 Weakness: fickle, gossipy, amphibian
 Symbol: Twins
@@ -49,6 +51,7 @@ Celebrities: John F. Kennedy, Queen Victoria
 Chinese zodiac twins: Horse"
 
     @cancer = "You chose Cancer! Let's see...\n
+Personality Traits: considerate, imaginative and sensitive
 Strength: with strong sixth sense, subjective, gentle, swift, imaginative, careful, dedicated, perseverant, kind, caring
 Weakness: greedy, possessive, sensitive, prim
 Symbol: Crab
@@ -62,6 +65,7 @@ Celebrities: Alexander the Great, Raul Gonzalez
 Chinese zodiac twins: Sheep"
 
     @leo = "You chose Leo! Let's see...\n
+Personality Traits: enthusiastic, proud and arrogant
 Strength: proud, charitable, reflective, loyal and enthusiastic
 Weakness: arrogant, vainglorious, indulgent, wasteful, willful, and self-complacent
 Symbol: Lion
@@ -72,10 +76,10 @@ Lucky Number: 19
 Jewelry: Gold
 Best Match: Aries, Sagittarius and Leo
 Celebrities: Napoleon Bonaparte, Deng Xiaoping, Alexander Dumas, Jennifer Lopez, Whitney Houston, Sarah Brightman
-See Also: Leo Horoscope in 2020  
 Chinese zodiac twins: Monkey"
 
     @virgo = "You chose Virgo! Let's see...\n
+Personality Traits: elegant, perfectionist and picky
 Strength: helping, elegant, perfectionist, modest, practical, clearheaded
 Weakness: picky, nosey, tortuous, confining
 Symbol: Virgin maiden
@@ -89,6 +93,7 @@ Celebrities: Warren Buffett, Kobe Bryant, Michael Jackson, Rebecca De Mornay, Ri
 Chinese zodiac twins: Rooster"
 
     @libra = "You chose Libra! Let's see...\n
+Personality Traits: equitable, charming and hesitant
 Strength: idealistic, equitable, just, strong social skills, aesthetic, charming, artistic, beautiful, kind-hearted
 Weakness: hesitant, narcissistic, lazy, perfunctory, freewheeling
 Symbol: Scales
@@ -102,6 +107,7 @@ Celebrities: Oscar Wilde, Hillary Duff, Italo Calvino, Evander Hoilrield
 Chinese zodiac twins: Dog"
 
     @scorpio = "You chose Scorpio! Let's see...\n
+Personality Traits: insightful, mysterious and suspicious
 Strength: mysterious, rational, intelligent, independent, intuitive, dedicated, insightful, charming, sensible
 Weakness: suspicious, fanatical, complicated, possessive, arrogant, self-willed, extreme
 Symbol: Scorpion
@@ -116,6 +122,7 @@ See Also: Scorpio Horoscope in 2020
 Chinese zodiac twins: Pig"
 
     @sagittarius = "You chose Sagittarius! Let's see...\n
+Personality Traits: unconstrained, lively and rash
 Strength: insightful, superior, rational, brave, beautiful, lively, lovely, optimistic
 Weakness: forgetful, careless, rash
 Symbol: Archer
@@ -129,6 +136,7 @@ Celebrities: Mark Twain, Beethoven, Taylor Swift, Britney Spears
 Chinese zodiac twins: Rat"
 
     @capricorn = "You chose Capricorn! Let's see...\n
+Personality Traits: perseverant, practical and lonely
 Strength: excellent, intelligent, practical, reliable, perseverant, generous, optimistic, cute, persistent
 Weakness: stubborn, lonely, and suspicious
 Symbol: Goat
@@ -142,6 +150,7 @@ Celebrities: Mao Zedong, Issac Newton, Martin Luther King, Nicholas Cage
 Chinese zodiac twins: Ox"
 
     @aquarius = "You chose Aquarius! Let's see...\n
+Personality Traits: smart, liberalistic and changeful
 Strength: original, tolerant, ideal, calm, friendly, charitable, independent, smart, practical
 Weakness: changeful, disobedient, liberalistic, hasty, rebel
 Symbol: Water carrier
@@ -155,6 +164,7 @@ Celebrities: Francis Bacon, Thomas Edison, Agyness Deyn, Paris Hilton
 Chinese zodiac twins: Tiger"
 
     @pisces = "You chose Pisces! Let's see...\n
+Personality Traits: romantic, kind and sentimental
 Strength: conscious, aesthetic, platonic, dedicated, kind, with good temper
 Weakness: recessive, sentimental, indecisive, unrealistic
 Symbol: Fish
@@ -166,5 +176,7 @@ Jewelry: Ivory Stone
 Best Match: Scorpio, Cancer and Capricorn
 Celebrities: George Washington, Zhou Enlai, Albert Einstein, Justin Bieber
 Chinese zodiac twins: Rabbit"
+
+@repeat = "\nWould you like to select another zodiac sign? /yes, /no"
   end
 end
