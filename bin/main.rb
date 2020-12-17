421753d4408dcd26a56bc4e1d2bcc02bb3f6ae5c
anapdh <ana_d_hubner@hotmail.com>
2020-12-17 18:33:04 -0300

    code optimizations

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,8 @@
 require_relative '../lib/bot.rb'
 require_relative '../lib/message.rb'
 
-p "Running your bot!"
+puts "Hello there... Your bot is running!\n
+Now you can access the webpage https://web.telegram.org/#/im?p=@AnaOfZodiacs_bot to start using the bot, or simply search for 'Ana of Zodiacs' in your Telegram search area.\n
+Press Ctrl + Z in your keyboard to stop executing."
 
 Bot.new
