e6f887aa29da48fa61ce824d2c5d7d767c02be17
anapdh <ana_d_hubner@hotmail.com>
2020-12-21 12:51:51 -0300

    remove parameter ENV from bot.new, update gemfile

--- bin/main.rb
+++ bin/main.rb
@@ -9,6 +9,6 @@ puts "Hello there... The bot is running!\n
 Now you can access the webpage https://web.telegram.org/#/im?p=@AnaOfZodiacs_bot to start using the bot, or simply search for 'Ana of Zodiacs' in your Telegram search area.\n
 Press Ctrl + Z in your keyboard to stop executing."
 
-Bot.new(ENV)
+Bot.new
 
 # rubocop:enable Layout/LineLength
